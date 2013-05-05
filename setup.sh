1059bb1d174fe272b55b98be99b693a5ba13ee95
Brandon Skari <brandon@skari.org>
2013-05-05 13:59:56 -0700

    Add base Firefox preferences and setup script

---
+++ setup.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+set -u
+set -e
+
+##################################
+# Fancy files that can do includes
+##################################
+
+bashrc_file="$HOME/.bashrc"
+if [ ! -f "$bashrc_file" ];
+then
+    echo 'source $HOME/.dotfiles/bashrc' > "$bashrc_file"
+else
+    echo "$bashrc_file" 'already exists'
+fi
+
+vimrc_file="$HOME/.vimrc"
+if [ ! -f "$vimrc_file" ];
+then
+    vimrc_string=$(cat <<VIMRC_STRING
+set expandtab    " Use spaces instead of tabs
+"set noexpandtab  " Use tabs instead of spaces
+
+call pathogen#infect() " Load pathogen plugin manager
+
+source $HOME/.dotfiles/vimrc
+VIMRC_STRING
+)
+    echo "$vimrc_string" > "$vimrc_file"
+else
+    echo "$vimrc_file" 'already exists'
+fi
+
+mysql_file="$HOME/.my.cnf"
+if [ ! -f "$mysql_file" ];
+then
+    echo '!include ~/.dotfiles/my.cnf' > "$mysql_file"
+else
+    echo "$mysql_file" 'already exists'
+fi
+
+########################
+# Dumb soft linked files
+########################
+
+function create_soft_link {
+    target="$1"
+    link_name="$2"
+
+    if [ ! -f "$link_name" ];
+    then
+        ln -s "$target" "$link_name"
+    else
+        echo 'Skipping linking ' "$link_name" ' because it already exists'
+    fi
+}
+
+firefox_directory="$HOME/.mozilla/firefox"
+if [ -d "$firefox_directory" ];
+then
+    default_profile_directory="$(ls -d $firefox_directory/*default)"
+    directory_count=$(echo "$default_profile_directory" | wc -l)
+    if [ "$directory_count" -eq 1 ];
+    then
+        create_soft_link "$HOME/.dotfiles/user.js" "$default_profile_directory/user.js"
+    else 
+        echo 'Unable to find Firefox default profile directory'
+    fi
+else
+    echo 'No Firefox directory found'
+fi
+
+for i in tmux.conf bcrc ;
+do
+    create_soft_link "$HOME/.dotfiles/$i" "$HOME/.$i"
+done
---
+++ user.js
@@ -0,0 +1,33 @@
+// Firefox preferences
+
+var preferences = {
+    // *******************
+    // ***** Privacy *****
+    // *******************
+
+    // Do not track
+    "privacy.donottrackheader.enabled": true,
+
+    // Disallow third party cookies
+    // 0 = allow all, 1 = only from originating server, 2 = none,
+    // 3 = third-party only allowed if the site had stored cookies from a
+    // previous visit (available on Firefox 22.0)
+    "network.cookie.cookieBehavior": 2,
+
+    // ********************
+    // ***** Security *****
+    // ********************
+
+    // Don't load HTTP resources when on an HTTPS site
+    "security.mixed_content.block_active_content": true,
+
+    // ***********************
+    // ***** Preferences *****
+    // ***********************
+
+    "browser.tabs.warnOnClose": false,
+};
+
+for (var key in preferences) {
+    user_pref(key, preferences[key]);
+}
