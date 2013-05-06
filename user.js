4ab52a9c315c85682c3f6a4e1c3cd91e2ad1825e
Brandon <bs@bs-Vostro-410.(none)>
2013-05-05 21:53:40 -0700

    Add plugins click to play

--- user.js
+++ user.js
@@ -1,33 +1,24 @@
 // Firefox preferences
 
-var preferences = {
-    // *******************
-    // ***** Privacy *****
-    // *******************
+// *******************
+// ***** Privacy *****
+// *******************
+user_pref("privacy.donottrackheader.enabled", true);
 
-    // Do not track
-    "privacy.donottrackheader.enabled": true,
+// Disallow third party cookies
+// 0 = allow all, 1 = only from originating server, 2 = none,
+// 3 = third-party only allowed if the site had stored cookies from a
+// previous visit (available on Firefox 22.0)
+user_pref("network.cookie.cookieBehavior", 2);
 
-    // Disallow third party cookies
-    // 0 = allow all, 1 = only from originating server, 2 = none,
-    // 3 = third-party only allowed if the site had stored cookies from a
-    // previous visit (available on Firefox 22.0)
-    "network.cookie.cookieBehavior": 2,
+// ********************
+// ***** Security *****
+// ********************
+// Don't load HTTP resources when on an HTTPS site
+user_pref("security.mixed_content.block_active_content", true);
 
-    // ********************
-    // ***** Security *****
-    // ********************
-
-    // Don't load HTTP resources when on an HTTPS site
-    "security.mixed_content.block_active_content": true,
-
-    // ***********************
-    // ***** Preferences *****
-    // ***********************
-
-    "browser.tabs.warnOnClose": false
-};
-
-for (var key in preferences) {
-    user_pref(key, preferences[key]);
-}
+// ***********************
+// ***** Preferences *****
+// ***********************
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("plugins.click_to_play", true);
