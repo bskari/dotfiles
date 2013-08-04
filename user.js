e68898b4fd2217326acf295a769a00ef5542222e
Brandon Skari <brandon@skari.org>
2013-08-03 20:45:22 -0700

    Only allow third-party cookies if site has been visited before

--- user.js
+++ user.js
@@ -9,7 +9,7 @@ user_pref("privacy.donottrackheader.enabled", true);
 // 0 = allow all, 1 = only from originating server, 2 = none,
 // 3 = third-party only allowed if the site had stored cookies from a
 // previous visit (available on Firefox 22.0)
-user_pref("network.cookie.cookieBehavior", 2);
+user_pref("network.cookie.cookieBehavior", 3);
 
 // ********************
 // ***** Security *****
