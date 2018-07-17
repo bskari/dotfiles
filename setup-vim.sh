3006c6ec823cedc5e54c4157f35f4bca4e0ca9eb
Brandon Skari <brandon@skari.org>
2018-07-17 13:01:18 -0600

    Add black

--- setup-vim.sh
+++ setup-vim.sh
@@ -23,6 +23,8 @@ maktaba='https://github.com/google/vim-maktaba'
 codefmt='https://github.com/google/vim-codefmt.git'
 # Automatically detect and set indent settings, i.e. tabs vs. spaces
 sleuth='https://github.com/tpope/vim-sleuth.git'
+# Opinionated Python formatting
+black='git://github.com/ambv/black.git'
 
 for repo in \
     "$codefmt" \
@@ -36,8 +38,21 @@ do
     folder=$(echo "$repo" | tr '/' '\n' | tail -n 1 | sed 's/\.git//')
     if [ ! -d "$folder" ];
     then
+        echo git clone "${repo}"
         git clone "$repo"
     fi
 done
+
+# Black is only supported on Python 3
+set +e
+vim --version | grep -q -i -- '+python3'
+if [ "$?" -eq 0 ] ;
+then
+    echo git clone "${black}"
+    git clone "${black}"
+else
+    echo 'Skipping black because Vim does not have Python 3 support'
+fi
+set -e
 
 popd
